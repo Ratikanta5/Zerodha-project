a33e4f63bc202ea6739db0f210f1e863756b2c34
Ratikanta <ratikantamohanty924@gmail.com>
2025-10-19 20:01:06 +0530

    added feature

---
+++ src/landing_page/products/ProductPage.jsx
@@ -0,0 +1,16 @@
+import Hero from "./Hero";
+import LeftSection from "./LeftSection";
+import RightSection from "./RightSection";
+import Universe from "./Universe";
+
+export default function productPage(){
+    return(
+        <>
+            <Hero/>
+            <LeftSection/>
+            <RightSection/>
+            <Universe/>
+        </>
+    )
+}
+
