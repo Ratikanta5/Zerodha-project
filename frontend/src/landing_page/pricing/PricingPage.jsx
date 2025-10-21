eb5f25b00928eff6ddfd13890062392ac0f1e06b
Ratikanta <ratikantamohanty924@gmail.com>
2025-10-21 19:40:53 +0530

    Initial commit for full project

---
+++ frontend/src/landing_page/pricing/PricingPage.jsx
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import OpenAccount from  "./Brokerage";
+
+export default function PricingPage(){
+    return(
+                
+        <>
+            <Hero/>
+            <OpenAccount/>
+        </>
+    )
+}
