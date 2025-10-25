07f1a618dda6162ffcfd276a9e19add8b7a69a08
Ratikanta <ratikantamohanty924@gmail.com>
2025-10-25 11:00:50 +0530

    dashboard component created

---
+++ dashboard/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import Dashboard from "./Dashboard";
+import TopBar from "./Topbar";
+
+
+export default function Home(){
+  return (
+    <>
+      <TopBar />
+      <Dashboard />
+    </>
+  );
+};
