625ca736fc26dbf42b2cbe400c2c1359354d4347
Ratikanta <ratikantamohanty924@gmail.com>
2025-10-25 11:20:25 +0530

    added

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,14 @@
+import {  Routes, Route } from "react-router-dom";
+import './App.css'
+import Home from "./components/Home";
+
+export default function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </>
+  )
+}
+
