5fe53752259093a4c2eec3f75eab4978ebe35d9c
Ratikanta <ratikantamohanty924@gmail.com>
2025-10-27 23:27:37 +0530

    mongodb connected

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,33 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+
+
+
+//connection with mongo db
+
+const db_url = process.env.MONGO_URL;
+
+main()
+.then(()=>{
+    console.log("database  connected");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect(db_url);
+}
+
+
+
+
+
+
 
 
 
